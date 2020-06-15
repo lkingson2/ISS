17bf019bdfdde6b24aa5fd09297b9d080957abf6
Luke Kingsley <lukekingsley@lukes-mbp.attlocal.net>
2020-06-14 19:10:56 -0700

    Updates

---
+++ precache-manifest.fa00f85c920280e57921bb580026aa0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44cdca6e06906ab4a2f5def74a8ea556",
+    "url": "/KaplanLabs/index.html"
+  },
+  {
+    "revision": "53421d7616ddfed3b72b",
+    "url": "/KaplanLabs/static/css/main.052d0e75.chunk.css"
+  },
+  {
+    "revision": "0bd4d9800b027618d9cb",
+    "url": "/KaplanLabs/static/js/2.cd82867e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KaplanLabs/static/js/2.cd82867e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53421d7616ddfed3b72b",
+    "url": "/KaplanLabs/static/js/main.81f92541.chunk.js"
+  },
+  {
+    "revision": "72f46b1d3398ef8c4b96",
+    "url": "/KaplanLabs/static/js/runtime-main.c36de573.js"
+  },
+  {
+    "revision": "3b0714dcbf21980630f32d1751f93f4c",
+    "url": "/KaplanLabs/static/media/space-shuttle.3b0714dc.svg"
+  }
+]);
